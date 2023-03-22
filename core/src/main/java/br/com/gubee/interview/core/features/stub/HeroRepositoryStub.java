34b4e541ff41699cc3d440fdab73e1d7d30589db
Daniel <daniel.marques@edu.unifil.br>
2023-03-22 10:16:49 -0300

    Hero and PowerStats Tests createdAt and updatedAt ok

--- core/src/main/java/br/com/gubee/interview/core/features/stub/HeroRepositoryStub.java
+++ core/src/main/java/br/com/gubee/interview/core/features/stub/HeroRepositoryStub.java
@@ -5,6 +5,7 @@ import br.com.gubee.interview.core.exception.ResourceNotFoundException;
 import br.com.gubee.interview.core.features.hero.HeroRepository;
 import br.com.gubee.interview.model.Hero;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,8 @@ public class HeroRepositoryStub implements HeroRepository {
     @Override
     public Hero create(Hero hero) {
         hero.setId(UUID.randomUUID());
+        hero.setCreatedAt(Instant.now());
+        hero.setUpdatedAt(Instant.now());
         inMemory.put(hero.getId(), hero);
         return inMemory.get(hero.getId());
     }
@@ -44,7 +47,10 @@ public class HeroRepositoryStub implements HeroRepository {
     @Override
     public Hero updateById(UUID id, Hero heroToUpdate) throws ResourceNotFoundException {
         if (inMemory.get(id) == null) throw new ResourceNotFoundException();
-        heroToUpdate.setId(id);
+        var olderHero = inMemory.get(id);
+        heroToUpdate.setId(olderHero.getId());
+        heroToUpdate.setCreatedAt(olderHero.getCreatedAt());
+        heroToUpdate.setUpdatedAt(Instant.now());
         inMemory.put(heroToUpdate.getId(), heroToUpdate);
         return inMemory.get(heroToUpdate.getId());
     }
--- core/src/main/java/br/com/gubee/interview/core/features/stub/PowerStatsRepositoryStub.java
+++ core/src/main/java/br/com/gubee/interview/core/features/stub/PowerStatsRepositoryStub.java
@@ -4,6 +4,7 @@ import br.com.gubee.interview.core.exception.ResourceNotFoundException;
 import br.com.gubee.interview.core.features.powerstats.PowerStatsRepository;
 import br.com.gubee.interview.model.PowerStats;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +17,8 @@ public class PowerStatsRepositoryStub implements PowerStatsRepository {
     @Override
     public PowerStats create(PowerStats powerStats) {
         powerStats.setId(UUID.randomUUID());
+        powerStats.setCreatedAt(Instant.now());
+        powerStats.setUpdatedAt(Instant.now());
         inMemory.put(powerStats.getId(), powerStats);
         return inMemory.get(powerStats.getId());
     }
@@ -41,7 +44,10 @@ public class PowerStatsRepositoryStub implements PowerStatsRepository {
     @Override
     public PowerStats updateById(UUID id, PowerStats powerStatsToUpdate) throws ResourceNotFoundException {
         if (inMemory.get(id) == null) throw new ResourceNotFoundException();
-        powerStatsToUpdate.setId(id);
+        var olderPowerStats = inMemory.get(id);
+        powerStatsToUpdate.setId(olderPowerStats.getId());
+        powerStatsToUpdate.setCreatedAt(olderPowerStats.getCreatedAt());
+        powerStatsToUpdate.setUpdatedAt(Instant.now());
         inMemory.put(powerStatsToUpdate.getId(), powerStatsToUpdate);
         return inMemory.get(powerStatsToUpdate.getId());
     }
